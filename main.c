db2be93f3317164529be1cea56fd3864e9f31bae
Tiago <78939124+TiagoCostaSantos@users.noreply.github.com>
2022-11-11 00:44:23 -0300

    Cadastro de Contas

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(void){
+
+    // ------------ CADASTRO DE USUARIOS -------------
+
+    struct ficha_user{
+
+        char usuario[30];
+        int senha;
+
+    };
+
+    struct ficha_user user;
+
+    printf("cadastro de usuario:\n");
+    printf("User: ");
+    fflush(stdin);
+
+    fgets(user.usuario, 20, stdin);
+    printf("Senha: ");
+    scanf("%d", &user.senha);
+
+    FILE *ArqLogin; // SALVANDO INFORMAÇÕES
+    int a;
+
+    ArqLogin = fopen("UsersLogin.txt", "a");
+
+    if(ArqLogin == NULL){
+
+        printf("Erro na abertura do arquivo! Por favor tente novamente mais tarde! :(\n"); // CASO SALVAMENTO DE DADOS DE ERRADO
+        system("pause");
+        return 1;
+
+    }else{
+
+        fprintf(ArqLogin, "%s%d\n", user.usuario, user.senha); // TELA APÓS CADASTRO COMPLETADO!
+        printf("\nUSUARIO CADASTRADO COM SUCESSO! \n");
+        printf("\nUsuario...: %s", user.usuario);
+        printf("senha.....: %d \n", user.senha);
+
+    }
+    system("pause");
+    return (0);
+
 }
